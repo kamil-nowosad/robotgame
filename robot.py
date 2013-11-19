da4079fb7c74255d14bd8895e05163e3dd7952cf
Kamil Nowosad <kamil.nowosad@gmail.com>
2013-11-19 17:15:49 +0100

    cleanups, bugfixes, nice collision detections (calculate moves of others)

--- robot.py
+++ robot.py
@@ -11,25 +11,20 @@ def sub2d(loc1, loc2):
 def wdist2d(loc1, loc2):
 	return (abs(loc1[0] - loc2[0]), abs(loc1[1] - loc2[1]))
 
-def towardFilteredOrCurrent(current_loc, dest_loc, filter_out = ('invalid', 'spawn')):
-	loc_dist = [{"location": loc, "distance": rg.wdist(loc, dest_loc)} for loc in rg.locs_around(current_loc, filter_out) + [current_loc]]
+def towardFilteredOrCurrent(current_loc, dest_loc):
+	filter_out = ['invalid']
+	if not 'spawn' in rg.loc_types(current_loc):
+		filter_out.append('spawn')
+	
+	goableLocations = [current_loc] + filter(lambda loc: filter(lambda l: l == loc, _game['robots'].keys()) == [], rg.locs_around(current_loc, filter_out))
+	loc_dist = [{"location": loc, "distance": rg.wdist(loc, dest_loc)} for loc in goableLocations]
 	loc_dist = sorted(loc_dist, key=lambda x: (x["distance"], max(wdist2d(x["location"], dest_loc))))
+	print loc_dist
 	return loc_dist[0]["location"]
-
-def canGoTo(location):
-	return rg.loc_types(location) == ['normal']
 
 def enemyAtLoc(location):
 	assert _game['robots'].has_key(location), "Expected robot at location %s, loc_types: %s" % (location, rg.loc_types(location))
 	return _game['robots'][location].player_id != _myPlayerId
-
-def simpleResolveCollision(robot, goToLoc):
-	if enemyAtLoc(goToLoc):
-		return ['attack', goToLoc]
-	if random.randint(0, 1) == 0:
-		return ['guard']
-	else:
-		return ['move', goToLoc]
 
 def minWDistFromEnemyRobot(location):
 	locationsDistances = map(lambda loc: {"location": loc, "distance": rg.wdist(location, loc)}, _gameView.locationsOfEnemyRobots)
@@ -42,7 +37,7 @@ def printBasicMove(currentLocation, basicMove):
 	return result
 
 class Params:
-	HP_RETREAT_THRESHOLD = 10 
+	HP_RETREAT_THRESHOLD = -1
 
 class Move:
 	def __init__(self, move, why):
@@ -77,6 +72,7 @@ class GameView:
 			enemy = min(self.enemies, key=lambda r: (r['numAttacking'] / 50, rg.wdist(myLoc, r['location'])))
 			self.targets[myRobot['location']] = enemy['robot']
 			enemy['numAttacking'] += 1
+		print self.targets
 
 class Walker:
 	def __init__(self, me):
@@ -86,13 +82,9 @@ class Walker:
 		newLocOrCurrent = towardFilteredOrCurrent(self.me.location, location)
 
 		if newLocOrCurrent != self.me.location:
-			if canGoTo(newLocOrCurrent):
-				return Move(['move', newLocOrCurrent], "Walker %s" % str(location))
-			else:
-				print "Being at %s cannot go to %s chasing robot at %s" % (self.me.location, newLocOrCurrent, robot.location)
-				return Move(simpleResolveCollision(self.me, newLocOrCurrent), "Walker %s (collision)" % str(location))
-		
-		return Move(['guard'], "Walker %s" % str(location))
+			return Move(['move', newLocOrCurrent], "Walker %s" % str(location))
+		else:		
+			return Move(['guard'], "Walker %s" % str(location))
 
 class Hunter:
 	def __init__(self, me):
@@ -101,6 +93,9 @@ class Hunter:
 	def hunt(self, robot):
 		if rg.wdist(self.me.location, robot.location) <= 1:
 			return Move(['attack', robot.location], "Hunter %s" % str(robot.location))
+		for enemyRobotLocation in _gameView.locationsOfEnemyRobots:
+			if rg.wdist(self.me.location, enemyRobotLocation) <= 1:
+				return Move(['attack', enemyRobotLocation], "Hunter [found other enemy] %s" % str(enemyRobotLocation))
 		
 		walker = Walker(self.me)
 		return Move(walker.goTowards(robot.location), "Hunter %s [needed to walk]" % str(robot.location))
@@ -122,8 +117,8 @@ def calculateMove(robot):
 	if 'spawn' in rg.loc_types(robot.location):
 		walker = Walker(robot)
 		return walker.goTowards(rg.CENTER_POINT)
-	
-	if robot.hp <= Params.HP_RETREAT_THRESHOLD:
+
+	if robot.hp <= Params.HP_RETREAT_THRESHOLD: # retreat turned off
 		retreater = Retreater(robot)
 		moveOrNone = retreater.tryRetreat()
 		if moveOrNone:
@@ -131,6 +126,29 @@ def calculateMove(robot):
 		
 	hunter = Hunter(robot)
 	return hunter.hunt(_gameView.targets[robot.location])
+
+class InternalRobot:
+	def __init__(self, location, hp):
+		self.location = location
+		self.hp = hp
+
+def possiblyFixMove(me, move):
+	basicMove = move.getBasicMove()
+	if basicMove[0] != "move":
+		return move
+
+	myOtherRobots = map(lambda (loc, robot): robot, filter(lambda (loc, robot): loc != me.location, _gameView.myRobots))
+	movesOfMyOtherRobots = map(lambda robot: {"robot": robot, "move": calculateMove(InternalRobot(robot["location"], robot["hp"]))}, myOtherRobots)
+
+	for otherMove in movesOfMyOtherRobots:
+		basicOtherMove = otherMove["move"].getBasicMove()
+		if basicOtherMove[0] != "move":
+			continue
+		if basicOtherMove[1] != basicMove[1]:
+			continue
+		if otherMove["robot"]["location"] >= me.location:
+			return Move(['guard'], "possiblyFixMoveOverridden [%s]" % move.getWhy())
+	return move
 
 class Robot:
 		def act(self, game):
@@ -144,6 +162,8 @@ class Robot:
 			_gameView.calculateTargets()
 
 			move = calculateMove(self)
+			move = possiblyFixMove(self, move)	
+
 			why = move.getWhy()
 			basicMove = move.getBasicMove()
 
